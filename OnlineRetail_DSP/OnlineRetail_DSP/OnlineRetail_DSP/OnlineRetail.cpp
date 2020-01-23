9075b62eae74e602f39b6667b4216b06bf2426db
senihcerit <ceritsenihm@gmail.com>
2020-01-23 23:38:39 +0300

    Versiyon 1

---
+++ OnlineRetail_DSP/OnlineRetail_DSP/OnlineRetail_DSP/OnlineRetail.cpp
@@ -0,0 +1,106 @@
+#include "stdafx.h"
+#include "OnlineRetail.h"
+#include <iostream>
+#include <fstream>
+
+OnlineRetail::OnlineRetail(int size) {
+	
+	sizeOfHashTable = size;
+
+	HashTable = new string[size];
+	DescriptionArray = new string[size];
+	QuantityArray = new int[size];
+
+	statusOfHashIndex = new bool[size];
+
+	for (auto i=0; i < size; i++)
+	{
+		statusOfHashIndex[i] = false;
+		QuantityArray[i] = 0;
+	}
+	
+		
+}
+
+OnlineRetail::~OnlineRetail() {
+	delete[] statusOfHashIndex;
+	delete[] HashTable;
+	delete[] DescriptionArray;
+	delete[] QuantityArray;
+}
+
+void OnlineRetail::ReadCsvAndFillHashTable(string filename) {
+	
+
+	fstream file(filename);
+
+	string InvoiceNo, StockCode, Description, Quantity, InvoiceDate, UnitPrice, CustomerID, Country;
+	
+	if (file.is_open()) {
+
+		while (file.good())
+		{
+			getline(file, InvoiceNo, ';');
+			getline(file, StockCode, ';');
+			getline(file, Description, ';');
+			getline(file, Quantity, ';');
+			getline(file, InvoiceDate, ';');
+			getline(file, UnitPrice, ';');
+			getline(file, CustomerID, ';');
+			getline(file, Country, '\n');
+
+			int asciiValue = int(StockCode.at(0));
+						
+			if ((Country == "United Kingdom") && (asciiValue >= 47) && (asciiValue <= 58)) {
+				
+				string convertedStockCode = StockCode.substr(0, 5);
+				Add(stoi(convertedStockCode) % sizeOfHashTable, StockCode, Description, stoi(Quantity));
+
+			}
+						
+		}
+	}
+}
+
+void OnlineRetail::Add(int hashIndex, const string& stockCode, const string& description, const int& quantity) {
+
+	auto p_count = 1;
+
+	while (statusOfHashIndex[hashIndex] != false && HashTable[hashIndex] != stockCode && p_count < sizeOfHashTable)
+	{
+		hashIndex = (hashIndex+1)% sizeOfHashTable;
+		p_count++;
+	}
+		
+	HashTable[hashIndex] = stockCode;
+	DescriptionArray[hashIndex] = description;
+	QuantityArray[hashIndex] += quantity;
+	statusOfHashIndex[hashIndex] = true;
+
+}
+
+void OnlineRetail::top10Analyzer() {
+	
+	cout << "Product(StockCode)" << "\t\tDescription" <<  "\t\t\t#TotalQuantity" << endl;
+
+	for (int i = 0; i < 10; i++) {
+		int topIndex = 0;
+		int topQuantity = QuantityArray[0];
+
+		for (int j = 0; j < sizeOfHashTable; j++) {
+
+			if (QuantityArray[j] > topQuantity) {
+				topQuantity = QuantityArray[j];
+
+				topIndex = j;
+
+			}
+		}
+
+		cout << i + 1 <<")  " << HashTable[topIndex] << "\t\t" << DescriptionArray[topIndex] << "\t\t\t" << topQuantity << endl;
+
+		QuantityArray[topIndex] = 0;
+
+	}
+	
+}
---
+++ OnlineRetail_DSP/OnlineRetail_DSP/OnlineRetail_DSP/OnlineRetail.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+
+using namespace std;
+
+class OnlineRetail {
+public:
+	
+	OnlineRetail(int); // Constructor for Online Retail System
+	~OnlineRetail();   // Destructor for Online Retail System
+
+	void ReadCsvAndFillHashTable(string); // Read Csv File, Create Hash Table Data Structure and Fill this HT
+
+	void Add(int, const string&, const string&, const int&); // Add items into Hash Table
+			
+	void top10Analyzer(); // Find top 10 products ordered
+	
+private:
+	
+	string *HashTable; //Hash Table pointer
+	
+	string *DescriptionArray; // *Description Array keeps descriptions of all products
+	
+	int *QuantityArray; // *Quantity Array keeps quantities of products. When program read csv file it adds quantities with previous.
+
+	bool *statusOfHashIndex; // This array keeps status of Hash Index. If there is item in hash index it will be true,
+	
+	int sizeOfHashTable; // Hash Table Size
+
+};
